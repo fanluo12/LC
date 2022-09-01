85e5e52f32486f055b2bce94582a854f8a2ebab6
Fan Luo <fluo4real@gmail.com>
2022-09-01 14:03:17 -0400

    Create 138. Copy List with Random Pointer.java

---
+++ LinkedList/138. Copy List with Random Pointer.java
@@ -0,0 +1,38 @@
+/*
+// Definition for a Node.
+class Node {
+    int val;
+    Node next;
+    Node random;
+
+    public Node(int val) {
+        this.val = val;
+        this.next = null;
+        this.random = null;
+    }
+}
+*/
+
+class Solution {
+    public Node copyRandomList(Node head) {
+        if (head == null) {
+            return null;
+        }
+        
+        HashMap<Node, Node> map = new HashMap<>();
+        Node cur = head;
+        
+        while (cur != null) {
+            map.put(cur, new Node(cur.val));
+            cur = cur.next;
+        }
+        
+        cur = head;
+        while (cur != null) {
+            map.get(cur).next = map.get(cur.next);
+            map.get(cur).random = map.get(cur.random);
+            cur = cur.next;
+        }
+        return map.get(head);
+    }
+}
