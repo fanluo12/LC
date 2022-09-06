51ef22997f06e8cd325296dfdeb58f97059b5efc
Fan Luo <fluo4real@gmail.com>
2022-09-06 14:08:39 -0400

    Create 199. Binary Tree Right Side View.java

---
+++ Trees/LevelOrder/199. Binary Tree Right Side View.java
@@ -0,0 +1,44 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> rightSideView(TreeNode root) {
+        if (root == null) {
+            return new ArrayList<>();
+        }
+        
+        Deque<TreeNode> queue = new ArrayDeque<>();
+        queue.offer(root);
+        List<Integer> res = new ArrayList<>();
+        
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i ++) {
+                TreeNode node = queue.poll();
+                if (i == size - 1) {
+                    res.add(node.val);
+                }
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+            }
+            
+        }
+        return res;
+    }
+}
