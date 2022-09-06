48a99c283f3f990716c0ae51d0a8f4e8a16b1d67
Fan Luo <fluo4real@gmail.com>
2022-09-06 13:47:12 -0400

    Create 543. Diameter of Binary Tree.java

---
+++ Trees/543. Diameter of Binary Tree.java
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int res = 0;
+    public int diameterOfBinaryTree(TreeNode root) {
+        getDepth(root);
+        return res;
+    }
+    
+    private int getDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int leftDepth = getDepth(root.left);
+        int rightDepth = getDepth(root.right);
+        res = Math.max(res, leftDepth + rightDepth);
+        
+        return Math.max(leftDepth, rightDepth) + 1;
+        
+    }
+}
