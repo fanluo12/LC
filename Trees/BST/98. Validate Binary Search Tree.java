f5c3ed6f6878bb9ec95fc3325d4782492435523d
Fan Luo <fluo4real@gmail.com>
2022-09-06 17:29:37 -0400

    Create 98. Validate Binary Search Tree.java

---
+++ Trees/BST/98. Validate Binary Search Tree.java
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isValidBST(TreeNode root) {
+        return valid(root, Long.MIN_VALUE, Long.MAX_VALUE);
+    }
+    
+    public boolean valid(TreeNode root, long left, long right) {
+        if (root == null) {
+            return true;
+        }
+        if (root.val <= left || root.val >= right) {
+            return false;
+        }
+        return valid(root.left, left, root.val) && valid(root.right, root.val, right);
+    }
+}
