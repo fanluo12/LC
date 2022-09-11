fe2c5187f6f0b91d6d889de303e22d9f68fa1648
Fan Luo <fluo4real@gmail.com>
2022-09-11 14:26:26 -0400

    Create 14. Longest Common Prefix.java

---
+++ String/14. Longest Common Prefix.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        
+        for (int i = 0; i < strs[0].length(); i ++) {
+            char c = strs[0].charAt(i);
+            for (int j = 0; j < strs.length; j ++) {
+                if (i >= strs[j].length() || c != strs[j].charAt(i)) {
+                    return strs[0].substring(0, i);
+                }
+            }
+        }
+        return strs[0];
+    }
+}
