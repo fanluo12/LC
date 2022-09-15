88409b36a94058f4e053f3596ceb57e2ce7f2b8a
Fan Luo <fluo4real@gmail.com>
2022-09-15 10:46:51 -0400

    Create HARD**114. Flatten Binary Tree to Linked List.java

---
+++ Trees/HARD**114. Flatten Binary Tree to Linked List.java
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public void flatten(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        TreeNode left = root.left;
+        TreeNode right = root.right;
+        root.left = null;
+        
+        flatten(left);
+        flatten(right);
+        
+        root.right = left;
+        while (root.right != null) {
+            root = root.right;
+        }
+        root.right = right;
+    }
+}
