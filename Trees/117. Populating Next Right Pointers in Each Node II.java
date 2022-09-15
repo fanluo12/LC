557b5e3fa32c6af6ff415fcdff4d3841c685d5af
Fan Luo <fluo4real@gmail.com>
2022-09-15 10:54:42 -0400

    Create 117. Populating Next Right Pointers in Each Node II.java

---
+++ Trees/117. Populating Next Right Pointers in Each Node II.java
@@ -0,0 +1,48 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node next;
+
+    public Node() {}
+    
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, Node _left, Node _right, Node _next) {
+        val = _val;
+        left = _left;
+        right = _right;
+        next = _next;
+    }
+};
+*/
+
+class Solution {
+    public Node connect(Node root) {
+        Node dummy = new Node(0);
+        Node pre = dummy;
+        Node returnRoot = root;
+        
+        while (root != null) {
+            if (root.left != null) {
+                pre.next = root.left;
+                pre = pre.next;
+            }
+            if (root.right != null) {
+                pre.next = root.right;
+                pre = pre.next;
+            }
+            root = root.next;
+            if (root == null) {
+                pre = dummy;
+                root = dummy.next;
+                dummy.next = null;
+            }
+        }
+        return returnRoot;
+    }
+}
