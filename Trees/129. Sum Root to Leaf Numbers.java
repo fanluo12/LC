1820273ede85260013a4fe7975b452449c0a3afb
Fan Luo <fluo4real@gmail.com>
2022-09-15 14:43:22 -0400

    Create 129. Sum Root to Leaf Numbers.java

---
+++ Trees/129. Sum Root to Leaf Numbers.java
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int sumNumbers(TreeNode root) {
+        return dfs(root, 0);
+    }
+    
+    private int dfs(TreeNode root, int sum) {
+        if (root == null) {
+            return 0;
+        }
+        sum = 10 * sum + root.val;
+        if (root.left == null && root.right == null) {
+            return sum;
+        }
+        return dfs(root.left, sum) + dfs(root.right, sum);
+    }
+}
