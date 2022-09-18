68e31e704cca244d7cfee0c4628d2cab3087a12f
Fan Luo <fluo4real@gmail.com>
2022-09-18 11:06:52 -0400

    Create 149. Max Points on a Line.java

---
+++ Arrays&Hashing/149. Max Points on a Line.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int maxPoints(int[][] points) {
+        int res = 1;
+        for (int i = 0; i < points.length; i ++) {
+            HashMap<Double, Integer> map = new HashMap<>();
+            for (int j = 0; j < points.length; j ++) {
+                if (i != j) {
+                    double k = Math.atan2(points[i][0] - points[j][0], points[i][1] - points[j][1]);
+                    map.put(k, map.getOrDefault(k, 1) + 1);
+                    res = Math.max(res, map.get(k));
+                }
+            }
+        }
+        return res;
+    }
+}
