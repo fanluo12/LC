ebf6eac24a83f6a2b115f8afd0bf4d1e689189c9
Fan Luo <fluo4real@gmail.com>
2022-09-25 13:35:17 -0400

    Update 367. Valid Perfect Square.java

--- BinarySearch/367. Valid Perfect Square.java
+++ BinarySearch/367. Valid Perfect Square.java
@@ -16,3 +16,24 @@ class Solution {
         return false;
     }
 }
+
+class Solution2 {
+    public boolean isPerfectSquare(int num) {
+        int left = 1, right = num;
+        
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            int res = num / mid, remainder = num % mid;
+            if (res == mid && remainder == 0) {
+                return true;
+            }
+            else if (res > mid) {
+                left = mid + 1;
+            }
+            else {
+                right = mid - 1;
+            }
+        }
+        return false;
+    }
+}
