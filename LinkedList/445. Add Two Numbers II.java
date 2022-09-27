d53755de05d8642ee3f3560235ac70e81f2be3c9
Fan Luo <fluo4real@gmail.com>
2022-09-27 13:42:13 -0400

    Update 445. Add Two Numbers II.java

--- LinkedList/445. Add Two Numbers II.java
+++ LinkedList/445. Add Two Numbers II.java
@@ -48,3 +48,34 @@ class Solution {
         return dummy.next;
     }
 }
+
+// Method like Question 2
+
+class Solution2 {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        Stack<Integer> stack1 = new Stack<>();
+        Stack<Integer> stack2 = new Stack<>();
+        
+        while (l1 != null) {
+            stack1.push(l1.val);
+            l1 = l1.next;
+        }
+        
+        while (l2 != null) {
+            stack2.push(l2.val);
+            l2 = l2.next;
+        }
+        
+        int carry = 0;
+        ListNode dummy = new ListNode(0);
+        
+        while (!stack1.empty() || !stack2.empty() || carry != 0) {
+            int sum = (stack1.empty() ? 0 : stack1.pop()) + (stack2.empty() ? 0 : stack2.pop()) + carry;
+            carry = sum / 10;
+            ListNode cur = new ListNode(sum % 10);
+            cur.next = dummy.next;
+            dummy.next = cur;
+        }
+        return dummy.next;
+    }
+}
