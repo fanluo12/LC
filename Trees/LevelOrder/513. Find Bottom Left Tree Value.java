6c67b5ec9fd38a0fa84c6c2b4d57ff6dab700308
Fan Luo <fluo4real@gmail.com>
2022-09-28 17:04:54 -0400

    Create 513. Find Bottom Left Tree Value.java

---
+++ Trees/LevelOrder/513. Find Bottom Left Tree Value.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int findBottomLeftValue(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        TreeNode node = new TreeNode();
+        Deque<TreeNode> queue = new ArrayDeque<>();
+        queue.offer(root);
+        
+        while (!queue.isEmpty()) {
+            node = queue.poll();
+            if (node.right != null) {
+                queue.offer(node.right);
+            }
+            if (node.left != null) {
+                queue.offer(node.left);
+            }
+        }
+        return node.val;
+    }
+}
