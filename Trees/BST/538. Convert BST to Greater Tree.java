4e479f05a0b347b1c2cdf6908194102dda7c0085
Fan Luo <fluo4real@gmail.com>
2022-09-28 20:38:23 -0400

    Create 538. Convert BST to Greater Tree.java

---
+++ Trees/BST/538. Convert BST to Greater Tree.java
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int sum = 0;
+    
+    public TreeNode convertBST(TreeNode root) {
+        dfs(root);
+        return root;
+    }
+    
+    public void dfs(TreeNode node) {
+        if (node == null) {
+            return;
+        }
+        dfs(node.right);
+        int tmp = node.val;
+        node.val += sum;
+        sum += tmp;
+        
+        dfs(node.left);
+    }
+}
