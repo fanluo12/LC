5402f7013ce1209df8065db22059ca7081a529dd
Fan Luo <fluo4real@gmail.com>
2022-09-29 11:25:02 -0400

    Create 560. Subarray Sum Equals K.java

---
+++ PrefixSum/560. Subarray Sum Equals K.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int subarraySum(int[] nums, int k) {
+        int prefixSum = 0, count = 0;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        map.put(0, 1);
+        
+        for (int n: nums) {
+            prefixSum += n;
+            if (map.containsKey(prefixSum - k)) {
+                count += map.get(prefixSum - k);
+            }
+            map.put(prefixSum,map.getOrDefault(prefixSum, 0) + 1);
+        }
+        return count;
+    }
+}
