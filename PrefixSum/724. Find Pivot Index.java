e061d64308f2182fdcbf67c20feb60a4389ad7bc
Fan Luo <fluo4real@gmail.com>
2022-09-30 16:23:50 -0400

    Create 724. Find Pivot Index.java

---
+++ PrefixSum/724. Find Pivot Index.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int pivotIndex(int[] nums) {
+        int sum = 0;
+        for (int n : nums) {
+            sum += n;
+        }
+        
+        int leftSum = 0;
+        
+        for (int i = 0; i < nums.length; i ++) {
+            int rightSum = sum - nums[i] - leftSum;
+            if (leftSum == rightSum) {
+                return i;
+            }
+            leftSum += nums[i];
+        }
+        return -1;
+    }
+}
