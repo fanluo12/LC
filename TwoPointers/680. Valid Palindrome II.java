17381f2a04995722002c078491a73f060b5bd254
Fan Luo <fluo4real@gmail.com>
2022-09-30 09:53:14 -0400

    Create 680. Valid Palindrome II.java

---
+++ TwoPointers/680. Valid Palindrome II.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean validPalindrome(String s) {
+        int l = 0, r = s.length() - 1;
+        
+        while (l < r) {
+            if (s.charAt(l) != s.charAt(r)) {
+                return (isPalindrome(s, l + 1, r) || isPalindrome(s, l, r - 1));
+            }
+            l ++;
+            r --;   
+        }
+        return true;
+    }
+    public boolean isPalindrome(String s, int l, int r) {
+        while (l < r) {
+            if (s.charAt(l) != s.charAt(r)){
+                return false;
+            }
+            l ++;
+            r --;
+        }
+        return true;
+    }
+}
