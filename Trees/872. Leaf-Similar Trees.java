dc511c2e33d9cd7ba22889750d7b42448750e194
Fan Luo <fluo4real@gmail.com>
2022-10-02 17:05:11 -0400

    Create 872. Leaf-Similar Trees.java

---
+++ Trees/872. Leaf-Similar Trees.java
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean leafSimilar(TreeNode root1, TreeNode root2) {
+        List<Integer> res1 = new ArrayList<>();
+        List<Integer> res2 = new ArrayList<>();
+        dfs(root1, res1);
+        dfs(root2, res2);
+        return res1.equals(res2);
+    }
+    
+    private void dfs(TreeNode root, List<Integer> res) {
+        if (root == null) {
+            return;
+        }
+        if (root.left == null && root.right == null) {
+            res.add(root.val);
+        }
+        dfs(root.left, res);
+        dfs(root.right, res);
+    }
+}
