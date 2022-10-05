427dc43e57f07d08821ba3f093cecbf028170e34
Fan Luo <fluo4real@gmail.com>
2022-10-05 11:13:29 -0400

    Create 1043. Partition Array for Maximum Sum.java

---
+++ 1D-DynamicProgramming/1043. Partition Array for Maximum Sum.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int maxSumAfterPartitioning(int[] arr, int k) {
+        int[] dp = new int[arr.length];
+        Arrays.fill(dp, 0);
+        int maxFirstK = arr[0];
+        
+        for (int i = 1; i < k; i ++) {
+            maxFirstK = Math.max(maxFirstK, arr[i]);
+            dp[i] = maxFirstK * (i + 1);
+        }
+        
+        for (int i = k; i < arr.length; i ++) {
+            int partitionMax = 0;
+            for (int j = 0; j < k; j ++) {
+                partitionMax = Math.max(partitionMax, arr[i - j]);
+                int prevSum = dp[i - j - 1];
+                dp[i] = Math.max(dp[i], prevSum + (j + 1) * partitionMax);
+            }
+        }
+        return dp[dp.length - 1];
+    }
+}
