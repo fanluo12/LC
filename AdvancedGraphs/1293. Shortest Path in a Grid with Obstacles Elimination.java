419cc8a923cc030feeb20236f02992d49272d85d
Fan Luo <fluo4real@gmail.com>
2022-10-07 20:18:49 -0400

    Create 1293. Shortest Path in a Grid with Obstacles Elimination.java

---
+++ AdvancedGraphs/1293. Shortest Path in a Grid with Obstacles Elimination.java
@@ -0,0 +1,47 @@
+class Solution {
+    
+    private static final int[][] directions = {{1, 0}, {-1, 0}, {0, -1}, {0, 1}};
+    
+    public int shortestPath(int[][] grid, int k) {
+        int ROWS = grid.length, COLS = grid[0].length;
+        Deque<int[]> queue = new LinkedList<>();
+        queue.offer(new int[]{0, 0, 0});
+        
+        int[][] visited = new int[ROWS][COLS];
+        for (int[] row: visited) {
+            Arrays.fill(row, Integer.MAX_VALUE);
+        }
+        visited[0][0] = 0;
+        
+        int distance = 0;
+        
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i ++) {
+                int[] tmp = queue.poll();
+                if (tmp[0] == ROWS - 1 && tmp[1] == COLS - 1) {
+                    return distance;
+                }
+                for (int[] dir: directions) {
+                    int newR = tmp[0] + dir[0];
+                    int newC = tmp[1] + dir[1];
+                    
+                    if (newR < 0 || newC < 0 || newR >= ROWS || newC >= COLS) {
+                        continue;
+                    }
+                    int newK = tmp[2] + grid[newR][newC];
+                    if (newK > k) {
+                        continue;
+                    }
+                    if (visited[newR][newC] <= newK) {
+                        continue;
+                    }
+                    visited[newR][newC] = newK;
+                    queue.offer(new int[]{newR, newC, newK});
+                }
+            }
+            distance ++;
+        }
+        return -1;
+    }
+}
