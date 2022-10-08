b25aff836b1aa46d29a9ec1d41aebcd59d906c5a
Fan Luo <fluo4real@gmail.com>
2022-10-08 17:59:02 -0400

    Create 1300. Sum of Mutated Array Closest to Target.java

---
+++ BinarySearch/1300. Sum of Mutated Array Closest to Target.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int findBestValue(int[] arr, int target) {
+        int max = Integer.MIN_VALUE, sum = 0;
+        for (int a : arr) {
+            sum += a;
+            max = Math.max(a, max);
+        }
+        
+        if (sum == target) {
+            return max;
+        }
+        
+        int min = 0, res = 1, diff = Integer.MAX_VALUE;
+        
+        while (min <= max) {
+            int mid = min + (max - min) / 2;
+            sum = getMutatedSum(arr, mid);
+            if (sum > target) {
+                max = mid - 1;
+            }
+            else {
+                min = mid + 1;
+            }
+            if (Math.abs(sum - target) < diff || (Math.abs(sum - target) == diff && mid < res)) {
+                res = mid;
+                diff = Math.abs(sum - target);
+            }
+        }
+        return res;
+        
+    }
+    
+    private int getMutatedSum(int[] arr, int mid) {
+        int sum = 0;
+        for (int a : arr) {
+            sum += (a > mid) ? mid : a;
+        }
+        return sum;
+    }
+}
