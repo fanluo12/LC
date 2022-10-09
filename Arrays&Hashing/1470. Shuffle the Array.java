043989d44ed17684b36d1a7e0cf54092adcbbde0
Fan Luo <fluo4real@gmail.com>
2022-10-09 14:29:04 -0400

    Create 1470. Shuffle the Array.java

---
+++ Arrays&Hashing/1470. Shuffle the Array.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int[] shuffle(int[] nums, int n) {
+        int[] res = new int[2 * n];
+        int mid = nums.length / 2;
+        
+        for (int i = 0; i < n; i ++) {
+            res[2 * i] = nums[i];
+            res[2 * i + 1] = nums[n + i];
+        }
+        return res;
+    }
+}
