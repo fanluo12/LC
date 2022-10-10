2e0fa98656e3510b895e287e61d28f94142898d2
Fan Luo <fluo4real@gmail.com>
2022-10-10 11:15:28 -0400

    Create 1512. Number of Good Pairs.java

---
+++ Arrays&Hashing/1512. Number of Good Pairs.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int numIdenticalPairs(int[] nums) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int res = 0;
+        
+        for (int n : nums) {
+            if (map.containsKey(n)) {
+                res += map.get(n);
+                map.put(n, map.getOrDefault(n, 0) + 1);
+            }
+            else {
+                map.put(n, 1);
+            }
+        }
+        return res;
+    }
+}
