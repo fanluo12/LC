0e17a7ba2e15424f7bdc38eccd2e681124e6916f
Fan Luo <fluo4real@gmail.com>
2022-10-13 13:21:59 -0400

    Update 167. Two Sum II - Input Array Is Sorted.java

--- TwoPointers/167. Two Sum II - Input Array Is Sorted.java
+++ TwoPointers/167. Two Sum II - Input Array Is Sorted.java
@@ -19,3 +19,22 @@ class Solution {
         return res;
     }
 }
+
+// updated version
+class Solution2 {
+    public int[] twoSum(int[] numbers, int target) {
+        int left = 0, right = numbers.length - 1;
+        while (left < right) {
+            if (numbers[left] + numbers[right] == target) {
+                return new int[]{left + 1, right + 1};
+            }
+            else if (numbers[left] + numbers[right] < target) {
+                left ++;
+            }
+            else {
+                right --;
+            }
+        }
+        return new int[2];
+    }
+}
