fad55ad30f6e778c054fa5bc8ec983059aca0bc2
Fan Luo <fluo4real@gmail.com>
2022-10-16 14:20:39 -0400

    Update 300. Longest Increasing Subsequence.java

--- 1D-DynamicProgramming/300. Longest Increasing Subsequence.java
+++ 1D-DynamicProgramming/300. Longest Increasing Subsequence.java
@@ -1,3 +1,4 @@
+// backward
 class Solution {
     public int lengthOfLIS(int[] nums) {
         int[] res = new int[nums.length];
@@ -9,6 +10,24 @@ class Solution {
                 if (nums[j] > nums[i]) {
                     res[i] = Math.max(res[i], 1 + res[j]);
                     max = Math.max(max, res[i]);
+                }
+            }
+        }
+        return max == 0 ? 1 : max;
+    }
+}
+// forwad
+class Solution2 {
+    public int lengthOfLIS(int[] nums) {
+        int[] dp = new int[nums.length];
+        Arrays.fill(dp, 1);
+        
+        int max = 0;
+        for (int i = 0; i < nums.length - 1; i ++) {
+            for (int j = i + 1; j < nums.length; j ++) {
+                if (nums[j] > nums[i]) {
+                    dp[j] = Math.max(dp[j], 1 + dp[i]);
+                    max = Math.max(max, dp[j]);
                 }
             }
         }
