aac525f39e2eda545ed3688959753431fda10ef3
Fan Luo <fluo4real@gmail.com>
2022-10-16 13:43:07 -0400

    Update 132. Palindrome Partitioning II.java

--- 1D-DynamicProgramming/132. Palindrome Partitioning II.java
+++ 1D-DynamicProgramming/132. Palindrome Partitioning II.java
@@ -6,8 +6,9 @@ class Solution {
         boolean[][] isPalindrome = new boolean[n][n];
         
         for (int right = 0; right < n; right ++) {
-            min = right; // max curs to make palindrome
+            min = right; // max cuts to make palindrome
             for (int left = 0; left <= right; left ++) {
+                // we already checked left == right, so if length < 3, it must be palindrome. or we shrink window to check palindrome
                 if (s.charAt(left) == s.charAt(right) && (right - left < 3 || isPalindrome[left + 1][right - 1])) {
                     isPalindrome[left][right] = true;
                     min = (left == 0 ? 0 : Math.min(min, dp[left - 1] + 1));
