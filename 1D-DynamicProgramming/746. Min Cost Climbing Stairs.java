36d407812c046d04b42494086e3515de4da0456a
Fan Luo <fluo4real@gmail.com>
2022-10-18 17:31:34 -0400

    Update 746. Min Cost Climbing Stairs.java

--- 1D-DynamicProgramming/746. Min Cost Climbing Stairs.java
+++ 1D-DynamicProgramming/746. Min Cost Climbing Stairs.java
@@ -1,3 +1,4 @@
+// backward
 class Solution {
     public int minCostClimbingStairs(int[] cost) {
         if (cost.length == 2) {
@@ -14,5 +15,29 @@ class Solution {
         }
         
         return Math.min(dp[0], dp[1]);
+    }
+}
+
+// forward
+class Solution {
+    public int minCostClimbingStairs(int[] cost) {
+        if (cost == null) {
+            return 0;
+        }
+        if (cost.length < 3) {
+            return cost.length == 2 ? Math.min(cost[0], cost[1]) : cost[0];
+        }
+        int n = cost.length;
+        int[] dp = new int[n + 1];
+        dp[n] = 0;
+        for (int i = 0; i < n; i ++) {
+            dp[i] = cost[i];
+        }
+        
+        for (int i = 2; i < dp.length; i ++) {
+            dp[i] += Math.min(dp[i - 2], dp[i - 1]);
+        }
+        
+        return Math.min(dp[dp.length - 1], dp[dp.length - 2]);
     }
 }
