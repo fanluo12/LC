8182e79b392cc2ba755242ba21d68ea7c516d459
Fan Luo <fluo4real@gmail.com>
2022-10-19 16:56:34 -0400

    Update 72. Edit Distance.java

--- 2D-DynamicProgramming/72. Edit Distance.java
+++ 2D-DynamicProgramming/72. Edit Distance.java
@@ -1,3 +1,14 @@
+/*
+  0 h o r s e
+0 0 1 2 3 4 5
+r 1 1 2 2 3 4
+o 2 2 1 2 3 4
+s 3 3 2 2 2 3
+we first initiate first row & col as indices representing how many would change.
+For first row, starting from index 1, once characters not matching, cumulately add 1 until matching. Note there left column, above row and left-up position
+we need to check to ensure minimum of them been added
+*/
+
 class Solution {
     public int minDistance(String word1, String word2) {
         int[][] dp = new int[word2.length() + 1][word1.length() + 1];
