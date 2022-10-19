90f3f0903cc05aa4951b4ac099bad396ce55c17e
Fan Luo <fluo4real@gmail.com>
2022-10-19 15:14:07 -0400

    Update 221. Maximal Square.java

--- 2D-DynamicProgramming/221. Maximal Square.java
+++ 2D-DynamicProgramming/221. Maximal Square.java
@@ -28,3 +28,26 @@ class Solution {
         return max * max;
     }
 }
+// methods similar to 1277
+class Solution2 {
+    public int maximalSquare(char[][] matrix) {
+        int max = 0, ROWS = matrix.length, COLS = matrix[0].length;
+        for (int r = 0; r < ROWS; r ++) {
+            max = Math.max(max, matrix[r][0] - '0');
+        }
+        
+        for (int c = 0; c < COLS; c ++) {
+            max = Math.max(max, matrix[0][c] - '0');
+        }
+        
+        for (int r = 1; r < ROWS; r ++) {
+            for (int c = 1; c < COLS; c ++) {
+                if (matrix[r][c] == '1' && matrix[r - 1][c] - '0' > 0 && matrix[r][c - 1] - '0' > 0 && matrix[r - 1][c - 1] - '0' > 0) {
+                    matrix[r][c] += Math.min(Math.min(matrix[r - 1][c] - '0', matrix[r][c - 1] - '0'), matrix[r - 1][c - 1] - '0');
+                }
+                max = Math.max(max, matrix[r][c] - '0');
+            }
+        }
+        return max * max;
+    }
+}
