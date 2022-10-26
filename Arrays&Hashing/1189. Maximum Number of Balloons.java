e8b936709fb78c6a960420f8ae61076bdc5d019f
Fan Luo <fluo4real@gmail.com>
2022-10-26 10:36:34 -0400

    Update 1189. Maximum Number of Balloons.java

--- Arrays&Hashing/1189. Maximum Number of Balloons.java
+++ Arrays&Hashing/1189. Maximum Number of Balloons.java
@@ -1,18 +1,20 @@
 class Solution {
     public int maxNumberOfBalloons(String text) {
-        int[] countText = new int[26];
-        int[] countBalloon = new int[26];
-        
+        int[] charText = new int[26];
+        int[] charBalloon = new int[26];
         for (char c: text.toCharArray()) {
-            countText[c - 'a'] += 1;
+            charText[c - 'a'] ++;
         }
         
-        for (char c: "balloon".toCharArray()) {
-            countBalloon[c - 'a'] += 1;
+        String ballon = "balloon";
+        for (char c: ballon.toCharArray()) {
+            charBalloon[c - 'a'] ++;
         }
-        int min = text.length();
-        for (char c: "balloon".toCharArray()) {
-            min = Math.min(min, countText[c - 'a'] / countBalloon[c - 'a']);
+        
+        int min = Integer.MAX_VALUE;
+        
+        for (char c: ballon.toCharArray()) {
+            min = Math.min(min, charText[c - 'a'] / charBalloon[c - 'a']);
         }
         return min;
     }
