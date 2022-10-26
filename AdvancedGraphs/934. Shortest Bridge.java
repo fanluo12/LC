3b377a1e468c6084aedfb9ff2b925a3ba3f4ff08
Fan Luo <fluo4real@gmail.com>
2022-10-26 10:11:49 -0400

    Update 934. Shortest Bridge.java

--- AdvancedGraphs/934. Shortest Bridge.java
+++ AdvancedGraphs/934. Shortest Bridge.java
@@ -1,4 +1,7 @@
 class Solution {
+    
+    private int[][] directions = {{0, 1}, {0, -1}, {-1, 0}, {1, 0}};
+    
     public int shortestBridge(int[][] grid) {
         boolean[][] visited = new boolean[grid.length][grid.length];
         for (int r = 0; r < grid.length; r ++) {
@@ -13,24 +16,23 @@ class Solution {
     }
     
     public void dfs(int[][] grid, int r, int c, boolean[][] visited) {
-        // dfs method to fill boolean array visited
-        int[][] directions = {{r - 1, c}, {r + 1, c}, {r, c + 1}, {r, c - 1}};
         if (r < 0 || c < 0 || r >= grid.length || c >= grid.length || grid[r][c] == 0 || visited[r][c]) {
             return;
         }
+        
         visited[r][c] = true;
         for (int[] dir: directions) {
-            dfs(grid, dir[0], dir[1], visited);
+            dfs(grid, r + dir[0], c + dir[1], visited);
         }
     }
     
     public int bfs(int[][] grid, boolean[][] visited) {
         int res = 0;
-        Deque<int[]> queue = new ArrayDeque<>();
-        for (int i = 0; i < grid.length; i ++) {
-            for (int j = 0; j < grid.length; j ++) {
-                if (visited[i][j]) {
-                    queue.offer(new int[]{i, j});
+        Deque<int[]> queue = new LinkedList<>();
+        for (int r = 0; r < grid.length; r ++) {
+            for (int c = 0; c < grid.length; c ++) {
+                if (visited[r][c]) {
+                    queue.offer(new int[]{r, c});
                 }
             }
         }
@@ -38,22 +40,24 @@ class Solution {
         while (!queue.isEmpty()) {
             int size = queue.size();
             for (int i = 0; i < size; i ++) {
-                int[] temp = queue.poll();
-                int r = temp[0], c = temp[1];
-                int[][] directions = {{r - 1, c}, {r + 1, c}, {r, c + 1}, {r, c - 1}};
+                int[] tmp = queue.poll();
+                int r = tmp[0], c = tmp[1];
                 for (int[] dir: directions) {
-                    if (dir[0] < 0 || dir[1] < 0 || dir[0] >= grid.length || dir[1] >= grid.length || visited[dir[0]][dir[1]]) {
+                    int newR = r + dir[0], newC =  c + dir[1];
+                    if (newR < 0 || newC < 0 || newR >= grid.length || newC >= grid.length || visited[newR][newC]) {
                         continue;
                     }
-                    if (grid[dir[0]][dir[1]] == 1) {
+                    if (grid[newR][newC] == 1) {
                         return res;
                     }
-                    queue.offer(new int[]{dir[0], dir[1]});
-                    visited[dir[0]][dir[1]] = true;
+                    visited[newR][newC] = true;
+                    queue.offer(new int[]{newR, newC});
                 }
             }
-            res += 1;
+            res ++;
         }
         return res;
     }
+    
+    
 }
