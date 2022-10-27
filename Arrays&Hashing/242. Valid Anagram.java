af6c4b8b324b6820d8c47454f65173d7af99ec86
Fan Luo <fluo4real@gmail.com>
2022-10-27 16:50:43 -0400

    Update 242. Valid Anagram.java

--- Arrays&Hashing/242. Valid Anagram.java
+++ Arrays&Hashing/242. Valid Anagram.java
@@ -3,7 +3,7 @@ class Solution {
         if (s.length() != t.length()) {
             return false;
         }
-        HashMap<Character, Integer> map = new HashMap<>();
+        Map<Character, Integer> map = new HashMap<>();
         for (int i = 0; i < s.length(); i ++) {
             map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0) + 1);
             map.put(t.charAt(i), map.getOrDefault(t.charAt(i), 0) - 1);
