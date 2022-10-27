8283cb5ee46d8c99ebf6878806a8643b18286b3a
Fan Luo <fluo4real@gmail.com>
2022-10-27 16:44:36 -0400

    Update and rename Arrays&Hashing/217. Contains Duplicate.java to Arrays&Hashing/easySet/217. Contains Duplicate.java

--- Arrays&Hashing/easySet/217. Contains Duplicate.java
+++ Arrays&Hashing/easySet/217. Contains Duplicate.java
@@ -1,6 +1,6 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        HashSet<Integer> set = new HashSet<>();
+        Set<Integer> set = new HashSet<>();
         for (int i: nums) {
             if (!set.add(i)) {
                 return true;
