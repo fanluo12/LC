05bf72f386cdf8bd2f9bba2354b45efebcebc5a2
Fan Luo <fluo4real@gmail.com>
2022-10-27 16:42:03 -0400

    Update 2001. Number of Pairs of Interchangeable Rectangles.java

--- Arrays&Hashing/2001. Number of Pairs of Interchangeable Rectangles.java
+++ Arrays&Hashing/2001. Number of Pairs of Interchangeable Rectangles.java
@@ -7,9 +7,29 @@ class Solution {
         }
         
         long res = 0;
+        // NOTE here to transfer integer to double
         for (double c: map.values()) {
             if (c > 1) {
                 res += c * (c - 1) / 2;
+            }
+        }
+        return res;
+    }
+}
+
+class Solution2 {
+    public long interchangeableRectangles(int[][] rectangles) {
+        Map<Double, Long> map = new HashMap<>();
+        
+        for (int[] rec: rectangles) {
+            double k = Math.atan2((double)rec[0], (double)rec[1]);
+            map.put(k, map.getOrDefault(k, (long)0) + 1);
+        }
+        
+        long res = 0;
+        for (long k: map.values()) {
+            if (k > 1) {
+                res += k * (k - 1) / 2;
             }
         }
         return res;
