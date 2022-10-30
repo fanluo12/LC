fb52a2b082d9260ac0952fa6bbcda8cb33a05438
Fan Luo <fluo4real@gmail.com>
2022-10-30 16:49:49 -0400

    Update and rename Arrays&Hashing/485. Max Consecutive Ones.java to Arrays&Hashing/easyArray/485. Max Consecutive Ones.java

--- Arrays&Hashing/easyArray/485. Max Consecutive Ones.java
+++ Arrays&Hashing/easyArray/485. Max Consecutive Ones.java
@@ -1,12 +1,12 @@
 class Solution {
     public int findMaxConsecutiveOnes(int[] nums) {
-        int count = 0;
-        int res = 0;
-        for (int n : nums) {
+        int count = 0, res = 0;
+        for (int n: nums) {
             if (n == 1) {
-                count += 1;
+                count ++;
                 res = Math.max(res, count);
-            } else {
+            }
+            else {
                 count = 0;
             }
         }
