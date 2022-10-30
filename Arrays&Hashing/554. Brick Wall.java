ed5b83c15d4df21bb2f6f4b43472cf53c5208ee4
Fan Luo <fluo4real@gmail.com>
2022-10-30 17:17:26 -0400

    Update 554. Brick Wall.java

--- Arrays&Hashing/554. Brick Wall.java
+++ Arrays&Hashing/554. Brick Wall.java
@@ -1,6 +1,6 @@
 class Solution {
     public int leastBricks(List<List<Integer>> wall) {
-        HashMap<Integer, Integer> map = new HashMap<>();
+        Map<Integer, Integer> map = new HashMap<>();
         map.put(0, 0); // prevent case [[1],[1],[1]]
         for (int i = 0; i < wall.size(); i ++) {
             int total = 0;
