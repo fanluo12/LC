33c2668b6852fbb7f34404e74031e465243eca2a
Fan Luo <fluo4real@gmail.com>
2022-11-03 16:02:46 -0400

    Update 131. Palindrome Partitioning.java

--- Backtracking/131. Palindrome Partitioning.java
+++ Backtracking/131. Palindrome Partitioning.java
@@ -1,29 +1,31 @@
 class Solution {
     public List<List<String>> partition(String s) {
         List<List<String>> res = new ArrayList<>();
-        backtrack(res, new ArrayList<>(), s, 0);
+        backtrack(s, res, new ArrayList<>(), 0);
         return res;
     }
     
-    public void backtrack(List<List<String>> res, List<String> path, String s, int start) {
+    public void backtrack(String s, List<List<String>> res, List<String> path, int start) {
         if (start == s.length()) {
             res.add(new ArrayList<>(path));
         }
-        
         for (int i = start; i < s.length(); i ++) {
             if (isPalindrome(s, start, i)) {
                 path.add(s.substring(start, i + 1));
-                backtrack(res, path, s, i + 1);
+                backtrack(s, res, path, i + 1);
                 path.remove(path.size() - 1);
+                
             }
         }
     }
     
     public boolean isPalindrome(String s, int low, int high) {
         while (low < high) {
-            if (s.charAt(low ++) != s.charAt(high --)) {
+            if (s.charAt(low) != s.charAt(high)) {
                 return false;
             }
+            low ++;
+            high --;
         }
         return true;
     }
