1b058b3fbe5320d8c615e951471f8f3198768dda
Fan Luo <fluo4real@gmail.com>
2022-11-03 17:36:58 -0400

    Update 22. Generate Parentheses.java

--- Backtracking/22. Generate Parentheses.java
+++ Backtracking/22. Generate Parentheses.java
@@ -1,4 +1,14 @@
 class Solution {
+    public String findDifferentBinaryString(String[] nums) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < nums.length; i ++) {
+            sb.append(nums[i].charAt(i) == '0' ? '1' : '0');
+        }
+        return sb.toString();
+    }
+}
+
+class Solution2 {
     public List<String> generateParenthesis(int n) {
         List<String> res = new ArrayList<>();
         backtrack("", res, n, n);
