260ae96cb24f87afe5bb0ee3a73a123214c361b0
Fan Luo <fluo4real@gmail.com>
2022-11-06 13:52:26 -0500

    Update 51. N-Queens.java

--- Backtracking/51. N-Queens.java
+++ Backtracking/51. N-Queens.java
@@ -1,20 +1,20 @@
 class Solution {
     public List<List<String>> solveNQueens(int n) {
         char[][] board = new char[n][n];
-        for (char[] boardRow: board) {
-            Arrays.fill(boardRow, '.');
+        for (char[] row: board) {
+            Arrays.fill(row, '.');
         }
         
-        HashSet<Integer> colSet = new HashSet<>();
-        HashSet<Integer> posDiagnalSet = new HashSet<>();
-        HashSet<Integer> negDiagnalSet = new HashSet<>();
+        Set<Integer> colSet = new HashSet<>();
+        Set<Integer> posDiagnalSet = new HashSet<>();
+        Set<Integer> negDiagnalSet = new HashSet<>();
         
         List<List<String>> res = new ArrayList<>();
         backtrack(0, n, board, colSet, posDiagnalSet, negDiagnalSet, res);
         return res;
     }
     
-    public void backtrack(int row, int n, char[][] board, HashSet<Integer> colSet, HashSet<Integer> posDiagnalSet, HashSet<Integer> negDiagnalSet, List<List<String>> res) {
+    public void backtrack(int row, int n, char[][] board, Set<Integer> colSet, Set<Integer> posDiagnalSet, Set<Integer> negDiagnalSet, List<List<String>> res) {
         if (row == n) {
             List<String> tmp = new ArrayList<>();
             for (char[] boardRow: board) {
