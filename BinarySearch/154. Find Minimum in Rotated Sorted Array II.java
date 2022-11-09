96140f04d7f9080cc614fcb431d89cf3cb7c23ce
Fan Luo <fluo4real@gmail.com>
2022-11-09 16:12:23 -0500

    Update 154. Find Minimum in Rotated Sorted Array II.java

--- BinarySearch/154. Find Minimum in Rotated Sorted Array II.java
+++ BinarySearch/154. Find Minimum in Rotated Sorted Array II.java
@@ -1,4 +1,29 @@
+// method similar to 153. Note we need to update left to find minimum and there is duplicate. When mid is greater or equal to left, we update left by +1 not left = mid + 1
 class Solution {
+    public int findMin(int[] nums) {
+        if (nums.length == 1) {
+            return nums[0];
+        }
+        
+        int left = 0, right = nums.length - 1;
+        
+        while (left < right) {
+            if (nums[left] < nums[right]) {
+                return nums[left];
+            }
+            int mid = left + (right - left) / 2;
+            if (nums[mid] >= nums[left]) {
+                left ++;
+            }
+            else {
+                right = mid;
+            }
+        }
+        return nums[left];
+    }
+}
+
+class Solution2 {
     public int findMin(int[] nums) {
         if (nums.length == 1) {
             return nums[0];
