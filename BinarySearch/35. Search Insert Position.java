a14887897f167b6a04c688d6214a1eaa3a09f474
Fan Luo <fluo4real@gmail.com>
2022-11-13 16:59:17 -0500

    Update 35. Search Insert Position.java

--- BinarySearch/35. Search Insert Position.java
+++ BinarySearch/35. Search Insert Position.java
@@ -14,3 +14,30 @@ class Solution {
         return left;
     }
 }
+
+// method 2 
+class Solution2 {
+    public int searchInsert(int[] nums, int target) {
+        if (target <= nums[0]) {
+            return 0;
+        }
+        else if (target > nums[nums.length - 1]) {
+            return nums.length;
+        }
+        
+        int left = 0, right = nums.length - 1;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            }
+            else if (nums[mid] < target) {
+                left = mid + 1;
+            }
+            else {
+                right = mid;
+            }
+        }
+        return left;
+    }
+}
