dfeeabbe4448e507c539a2011de6261eb567ed47
Fan Luo <fluo4real@gmail.com>
2022-11-20 14:34:39 -0500

    Update 542. 01 Matrix.java

--- Deque/542. 01 Matrix.java
+++ Deque/542. 01 Matrix.java
@@ -1,14 +1,16 @@
 class Solution {
     public int[][] updateMatrix(int[][] mat) {
-        Deque<int[]> queue = new ArrayDeque<>();
-        int[][] res = new int[mat.length][mat[0].length];
+        int ROWS = mat.length, COLS = mat[0].length;
+        int[][] res = new int[ROWS][COLS];
         int[][] directions = {{-1, 0}, {1, 0}, {0, 1}, {0, -1}};
+        
         for (int[] row: res) {
             Arrays.fill(row, -1);
         }
         
-        for (int r = 0; r < mat.length; r ++) {
-            for (int c = 0; c < mat[0].length; c ++) {
+        Deque<int[]> queue = new LinkedList<>();
+        for (int r = 0; r < ROWS; r ++) {
+            for (int c = 0; c < COLS; c ++) {
                 if (mat[r][c] == 0) {
                     res[r][c] = 0;
                     queue.offer(new int[]{r, c});
@@ -19,10 +21,10 @@ class Solution {
         while (!queue.isEmpty()) {
             int[] tmp = queue.poll();
             for (int[] dir: directions) {
-                int newRow = tmp[0] + dir[0], newCol = tmp[1] + dir[1];
-                if (0 <= newRow && 0 <= newCol && newRow < mat.length && newCol < mat[0].length && res[newRow][newCol] == -1) {
-                    res[newRow][newCol] = res[tmp[0]][tmp[1]] + 1;
-                    queue.offer(new int[]{newRow, newCol});
+                int newR = tmp[0] + dir[0], newC = tmp[1] + dir[1];
+                if (0 <= newR && newR < ROWS && 0 <= newC && newC < COLS && res[newR][newC] == -1) {
+                    res[newR][newC] = res[tmp[0]][tmp[1]] + 1;
+                    queue.offer(new int[]{newR, newC});
                 }
             }
         }
