9f446d41ff718f1b6baf8a8cb63f63d8e0cde464
Fan Luo <fluo4real@gmail.com>
2022-11-21 20:48:08 -0500

    Update 1091. Shortest Path in Binary Matrix.java

--- Graphs/1091. Shortest Path in Binary Matrix.java
+++ Graphs/1091. Shortest Path in Binary Matrix.java
@@ -4,23 +4,25 @@ class Solution {
         if (grid[0][0] == 1 || grid[n - 1][n - 1] == 1) {
             return -1;
         }
-        Deque<int[]> queue = new ArrayDeque<>();
+        
+        int[][] directions = {{-1, 0}, {1, 0}, {0, 1}, {0, -1}, {-1, -1}, {-1, 1}, {1, -1}, {1, 1}};
+        
+        Deque<int[]> queue = new LinkedList<>();
         queue.offer(new int[]{0, 0, 1});
-        grid[0][0] = 1;
         
         while (!queue.isEmpty()) {
             int size = queue.size();
             for (int i = 0; i < size; i ++) {
-                int[] temp = queue.poll();
-                int r = temp[0], c = temp[1], path = temp[2];
+                int[] tmp = queue.poll();
+                int r = tmp[0], c = tmp[1], path = tmp[2];
                 if (r == n - 1 && c == n - 1) {
                     return path;
                 }
-                int[][] directions = {{r - 1, c}, {r + 1, c}, {r, c + 1}, {r, c - 1}, {r - 1, c - 1}, {r - 1, c + 1}, {r + 1, c - 1}, {r + 1, c + 1}};
                 for (int[] dir: directions) {
-                    if (0 <= dir[0] && 0 <= dir[1] && dir[0] < n && dir[1] < n && grid[dir[0]][dir[1]]  == 0) {
-                        grid[dir[0]][dir[1]] = 1;
-                        queue.offer(new int[]{dir[0], dir[1], path + 1});
+                    int newR = r + dir[0], newC = c + dir[1];
+                    if (0 <= newR && newR < n && 0 <= newC && newC < n && grid[newR][newC] == 0) {
+                        grid[newR][newC] = 1;
+                        queue.offer(new int[]{newR, newC, path + 1});
                     }
                 }
             }
