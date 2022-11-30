ccb6b4093a11f726a1e5207d00db60819ac42603
Fan Luo <fluo4real@gmail.com>
2022-11-30 16:27:33 -0500

    Update 329. Longest Increasing Path in a Matrix.java

--- Graphs/329. Longest Increasing Path in a Matrix.java
+++ Graphs/329. Longest Increasing Path in a Matrix.java
@@ -1,15 +1,17 @@
 class Solution {
     public int longestIncreasingPath(int[][] matrix) {
         int ROWS = matrix.length, COLS = matrix[0].length;
-        int max = 0;
+        
+        boolean[][] visited = new boolean[ROWS][COLS];
         int[][] dp = new int[ROWS][COLS];
-        int[][] visited = new int[ROWS][COLS];
+        
         for (int r = 0; r < ROWS; r ++) {
             for (int c = 0; c < COLS; c ++) {
-                dfs(matrix, dp, visited, -1, r, c);
+                dfs(matrix, r, c, dp, visited, -1);
             }
         }
         
+        int max = 0;
         for (int r = 0; r < ROWS; r ++) {
             for (int c = 0; c < COLS; c ++) {
                 max = Math.max(max, dp[r][c]);
@@ -18,21 +20,21 @@ class Solution {
         return max;
     }
     
-    public int dfs(int[][] matrix, int[][] dp, int[][] visited, int prev, int r, int c) {
+    public int dfs(int[][] matrix, int r, int c, int[][] dp, boolean[][] visited, int prev) {
         if (r < 0 || c < 0 || r >= matrix.length || c >= matrix[0].length || matrix[r][c] <= prev) {
             return 0;
         }
-        if (visited[r][c] == 1) {
+        if (visited[r][c]) {
             return dp[r][c];
         }
-        int count = 1;
-        count = Math.max(count, 1 + dfs(matrix, dp, visited, matrix[r][c], r + 1, c));
-        count = Math.max(count, 1 + dfs(matrix, dp, visited, matrix[r][c], r - 1, c));
-        count = Math.max(count, 1 + dfs(matrix, dp, visited, matrix[r][c], r, c + 1));
-        count = Math.max(count, 1 + dfs(matrix, dp, visited, matrix[r][c], r, c - 1));
         
-        dp[r][c] = count;
-        visited[r][c] = 1;
-        return count;
+        int path = 1;
+        path = Math.max(path, dfs(matrix, r + 1, c, dp, visited, matrix[r][c]) + 1);
+        path = Math.max(path, dfs(matrix, r - 1, c, dp, visited, matrix[r][c]) + 1);
+        path = Math.max(path, dfs(matrix, r, c + 1, dp, visited, matrix[r][c]) + 1);
+        path = Math.max(path, dfs(matrix, r, c - 1, dp, visited, matrix[r][c]) + 1);
+        visited[r][c] = true;
+        dp[r][c] = path;
+        return path;
     }
 }
