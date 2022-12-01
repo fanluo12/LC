e14df6be7479bbaad67f16010b0edcaf056fea8d
Fan Luo <fluo4real@gmail.com>
2022-11-30 19:28:14 -0500

    Update 547. Number of Provinces.java

--- Graphs/547. Number of Provinces.java
+++ Graphs/547. Number of Provinces.java
@@ -1,23 +1,21 @@
 class Solution {
     public int findCircleNum(int[][] isConnected) {
-        int[] visited = new int[isConnected.length];
+        boolean[] visited = new boolean[isConnected.length];
         int count = 0;
-        
         for (int i = 0; i < isConnected.length; i ++) {
-            if (visited[i] == 0) {
-                dfs(isConnected, visited, i);
-                count += 1;
+            if (!visited[i]) {
+                dfs(isConnected, i, visited);
+                count ++;
             }
         }
         return count;
     }
     
-    private void dfs(int[][] isConnected, int[] visited, int i) {
-        
+    public void dfs(int[][] isConnected, int i, boolean[] visited) {
         for (int j = 0; j < isConnected.length; j ++) {
-            if (visited[j] == 0 && isConnected[i][j] == 1) {
-                visited[j] = 1;
-                dfs(isConnected, visited, j);
+            if (!visited[j] && isConnected[i][j] == 1) {
+                visited[j] = true;
+                dfs(isConnected, j, visited);
             }
         }
     }
