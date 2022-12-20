9982b81cd395baec0992597c402877a52e2a7d32
Fan Luo <fluo4real@gmail.com>
2022-12-19 21:25:06 -0500

    Update 1. Two Sum.java

--- Arrays&Hashing/1. Two Sum.java
+++ Arrays&Hashing/1. Two Sum.java
@@ -1,6 +1,6 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        HashMap<Integer, Integer> map = new HashMap<>();
+        Map<Integer, Integer> map = new HashMap<>();
         
         for (int i = 0; i < nums.length; i ++) {
             if (map.containsKey(target - nums[i])) {
