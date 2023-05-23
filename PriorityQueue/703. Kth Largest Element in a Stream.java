4a004621b4db30959bdc9bad75df5874182458d2
Fan Luo <fluo4real@gmail.com>
2023-05-23 12:49:22 -0400

    Create 703. Kth Largest Element in a Stream.java

---
+++ PriorityQueue/703. Kth Largest Element in a Stream.java
@@ -0,0 +1,29 @@
+class KthLargest {
+
+    PriorityQueue<Integer> pq;
+    int k;
+
+    public KthLargest(int k, int[] nums) {
+        this.k = k;
+        pq = new PriorityQueue<>();
+        for (int n: nums) {
+            add(n);
+        }
+    }
+    
+    public int add(int val) {
+        if (pq.size() < k || val > pq.peek()) {
+            pq.offer(val);
+        }
+        if (pq.size() > k) {
+            pq.poll();
+        }
+        return pq.peek();
+    }
+}
+
+/**
+ * Your KthLargest object will be instantiated and called as such:
+ * KthLargest obj = new KthLargest(k, nums);
+ * int param_1 = obj.add(val);
+ */
